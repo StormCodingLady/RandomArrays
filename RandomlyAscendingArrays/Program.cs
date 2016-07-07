ad98aa36d44dd3a3bf207a384439c8f53590fc6f
stormiarias <sanduagaarias@gmail.com>
2016-07-07 12:39:43 -0700

    initial commit

---
+++ RandomlyAscendingArrays/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomlyAscendingArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray;
+            myArray = new int[7];
+            Random rnd = new Random();
+
+            for (int i = 0; i < 7; i++)
+            {
+                
+                int number = rnd.Next(100);
+                myArray[i] = number;
+                Console.WriteLine(myArray[i]);
+                string message = "Position # " + i + " contains " + myArray[i];
+                Console.WriteLine(message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
